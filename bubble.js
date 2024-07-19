2f2432e6b6b7dfb7ac4b0ea4f8bf84e727608bec
Nathan Soucier <soucienp@gmail.com>
2024-07-19 14:12:00 -0400

    completed bubble sort

--- bubble.js
+++ bubble.js
@@ -1,3 +1,19 @@
-function bubbleSort() {}
+function bubbleSort(arr) {
+    console.log('in here')
+    for (let j=arr.length; j > 0; j--) {
+        console.log(arr)
+        for (let i=0; i < j-1; i++) {
+            if (arr[i] > arr[i+1]) {
+                let temp = arr[i+1];
+                arr[i+1] = arr[i];
+                arr[i] = temp;
+
+            }
+        }
+    }
+    console.log('hereeeeeeeee')
+
+    return arr
+}
 
 module.exports = bubbleSort;
