7c875e7dbcb1554bd48c4677b9184692b1d4c635
Nathan Soucier <soucienp@gmail.com>
2024-07-23 11:54:23 -0400

    finished mergeSort algorithm

--- bubble.js
+++ bubble.js
@@ -1,7 +1,5 @@
 function bubbleSort(arr) {
-    console.log('in here')
     for (let j=arr.length; j > 0; j--) {
-        console.log(arr)
         for (let i=0; i < j-1; i++) {
             if (arr[i] > arr[i+1]) {
                 let temp = arr[i+1];
@@ -11,7 +9,6 @@ function bubbleSort(arr) {
             }
         }
     }
-    console.log('hereeeeeeeee')
 
     return arr
 }
--- merge.js
+++ merge.js
@@ -1,5 +1,41 @@
-function merge() {}
+function merge(arr1, arr2) {
+    const arrSorted = [];
+    let pointer1 = 0;
+    let pointer2 = 0;
 
-function mergeSort() {}
+    while (pointer1 < arr1.length && pointer2 < arr2.length) {
+        if (arr1[pointer1] < arr2[pointer2]) {
+            arrSorted.push(arr1[pointer1]);
+            pointer1++;
+        } else {
+            arrSorted.push(arr2[pointer2]);
+            pointer2++;
+        }
+    }
+    
+    if (pointer1 === arr1.length) {
+        while (pointer2 < arr2.length) {
+            arrSorted.push(arr2[pointer2]);
+            pointer2++;
+        }
+    } else {
+        while (pointer1 < arr1.length) {
+            arrSorted.push(arr1[pointer1]);
+            pointer1++;
+        }
+    }
+
+    return arrSorted
+}
+
+function mergeSort(arr) {
+    if (arr[0] === undefined) return []
+    if (arr.length === 1) return arr
+    let midPoint = Math.floor(arr.length/2);
+    let left = mergeSort(arr.splice(0,midPoint));
+    let right = mergeSort([...arr]);
+    
+    return merge(left, right);
+}
 
 module.exports = { merge, mergeSort};
